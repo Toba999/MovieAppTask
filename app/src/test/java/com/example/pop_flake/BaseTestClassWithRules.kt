ca712e670fe43532a2ed3d854251a43db3806bda
mohamed toba <mohamedtowba298@gmail.com>
2023-05-30 14:12:46 +0300

    adding base test classes

---
+++ app/src/test/java/com/example/pop_flake/BaseTestClassWithRules.kt
@@ -0,0 +1,77 @@
+package com.example.pop_flake
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
+
+
+open class BaseTestClassWithRules :BaseTest(){
+
+
+    @get: Rule
+    val androidComponentRule = InstantTaskExecutorRule()
+
+
+    fun invokePrivateMethod(instance:Any,methodName:String,prams:Array<Class<*>>?=null
+                            , pramsValues:Array<Any?>?=null):Any? {
+        val pramsAvailable = prams != null && pramsValues != null
+        val method =if(pramsAvailable)
+            instance::class.java.getDeclaredMethod(methodName,*(prams!!))
+        else instance::class.java.getDeclaredMethod(methodName)
+        method.isAccessible = true
+        return if(pramsAvailable)
+            method.invoke(instance,*(pramsValues!!))
+        else
+            method.invoke(instance)
+    }
+
+    fun setPrivateField(instance: Any,name:String,value:Any?){
+        val field = try {
+            instance::class.java.getDeclaredField(name)
+        }catch (e:NoSuchFieldException){
+            instance::class.java.superclass?.getDeclaredField(name)?:throw e
+        }
+        field.isAccessible =true
+        when(value){
+            is Boolean ->field.setBoolean(instance,value)
+            is Int ->field.setInt(instance , value)
+            is Byte ->field.setByte(instance , value)
+            is Char ->field.setChar(instance , value)
+            is Double ->field.setDouble(instance , value)
+            is Float ->field.setFloat(instance , value)
+            is Long ->field.setLong(instance , value)
+            is Short ->field.setShort(instance , value)
+            else ->field.set(instance , value)
+        }
+    }
+
+    fun getPrivateField(instance: Any,name:String): Any? {
+        val field = try {
+            instance::class.java.getDeclaredField(name)
+        }catch (e:NoSuchFieldException){
+            instance::class.java.superclass?.getDeclaredField(name)?:throw e
+        }
+        field.isAccessible =true
+        return field.get(instance)
+    }
+
+    @Throws(Exception::class)
+    fun setFinalIntStatic(field: Field, value: Any) {
+        field.isAccessible = true
+        val modifiersField: Field = Field::class.java.getDeclaredField("modifiers")
+        modifiersField.isAccessible = true
+        modifiersField.setInt(field, field.modifiers and Modifier.FINAL.inv())
+        when(value){
+            is Boolean ->field.setBoolean(null,value)
+            is Int ->field.setInt(null , value)
+            is Byte ->field.setByte(null , value)
+            is Char ->field.setChar(null , value)
+            is Double ->field.setDouble(null , value)
+            is Float ->field.setFloat(null , value)
+            is Long ->field.setLong(null , value)
+            is Short ->field.setShort(null , value)
+            else ->field.set(null , value)
+        }
+    }
+}
--- app/src/test/java/com/example/pop_flake/settingsTest/SettingsViewModelTest.kt
+++ app/src/test/java/com/example/pop_flake/settingsTest/SettingsViewModelTest.kt
@@ -1,35 +1,23 @@
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.pop_flake.BaseTestClassWithRules
 import com.example.pop_flake.data.local.DataStoreManager
-import com.example.pop_flake.data.pojo.ComplaintModel
-import com.example.pop_flake.data.pojo.SettingsState
 import com.example.pop_flake.ui.settings.SettingsViewModel
-import com.example.pop_flake.utils.Constants.DARK_MODE_KEY
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnit
-import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.junit.MockitoRule
 
 
 @ExperimentalCoroutinesApi
-class SettingsViewModelTest {
+class SettingsViewModelTest : BaseTestClassWithRules() {
 
     @get:Rule
     val mockitoRule: MockitoRule = MockitoJUnit.rule()
@@ -57,41 +45,6 @@ class SettingsViewModelTest {
         testScope.cleanupTestCoroutines()
     }
 
-    @Test
-    fun testGetDarkModeOption() = testScope.runTest {
-        val testValue = AppCompatDelegate.MODE_NIGHT_YES
-        val mockState = SettingsState(darkModeOption = testValue)
-        val flow = flowOf(testValue)
 
-        `when`(mockDataStoreManager.getInt(DARK_MODE_KEY, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)).thenReturn(flow)
-
-        viewModel.getDarkModeOption()
-
-        testScheduler.apply { advanceTimeBy(1); runCurrent() }
-
-        verify(mockDataStoreManager).getInt(DARK_MODE_KEY, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-        assert(viewModel.state.first() == mockState)
-    }
-
-    @Test
-    fun testAddComplaintItem() {
-        val mockComplaintModel = ComplaintModel("Test Complaint","desc")
-        val updatedState = SettingsState(list = mutableListOf(mockComplaintModel))
-
-        viewModel._state.value = viewModel._state.value.copy(list = mutableListOf(mockComplaintModel))
-
-        viewModel.addComplaintItem(mockComplaintModel)
-
-        assert(viewModel.state.value == updatedState)
-    }
-
-    @Test
-    fun testSetDarkModeOption() = testScope.runBlockingTest {
-        val testOption = AppCompatDelegate.MODE_NIGHT_NO
-
-        viewModel.setDarkModeOption(testOption)
-
-        verify(mockDataStoreManager, times(1)).setInt(DARK_MODE_KEY, testOption)
-    }
 
 }
